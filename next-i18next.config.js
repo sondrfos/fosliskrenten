ee57d1a6908e86f3a9b946215a58e8793e50b66a
sondrfos <sondre.foslien@gmail.com>
2023-05-18 21:16:48 +0000

    change to only norwegian

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE'],
+    defaultLocale: 'nb-NO',
+    locales: ['nb-NO'],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
